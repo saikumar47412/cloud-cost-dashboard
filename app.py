ca950cac5543da5e082a56567c4d3fe2230a4d63
saikumar47412 <55438954+saikumar47412@users.noreply.github.com>
2026-03-14 19:01:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def get_data():
         return jsonify({"error": f"Error fetching data: {str(e)}"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os port = int(os.environ.get("PORT", 5000)) app.run(host="0.0.0.0", port=port)
